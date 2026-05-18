9311f8767e19e9ade747cde5d78e47dea02f033f
Lucaskz <lucascruzdias8@gmail.com>
2026-05-18 16:27:00 -0300

    Mexendo na responsividade para o celular

---
+++ javascript/global.js
@@ -0,0 +1,14 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    navLinks.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', () => {
+        menuToggle.classList.remove('active');
+        navLinks.classList.remove('active');
+    });
+});
